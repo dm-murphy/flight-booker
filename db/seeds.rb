3265ebc455f48171c571fe6c36e0517d9160e8f4
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-02 12:52:08 -0500

    Add seed data for second Flight and takeoff times for flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,18 @@ p "First airport id? #{airports.first.id}"
 p "Last airport id? #{airports.last.id}"
 
 # flights = Flight.create(takeoff: '2021-12-9T04:05:06+00:00', departing_airport_id: 19, arriving_airport_id: 22)
-flights = Flight.create!([{
-            departing_airport_id: Airport.find_by!(name: "SFO").id,
-            arriving_airport_id: Airport.find_by!(name: "ORD").id
-          }])
+flights = Flight.create!([
+            {
+              departing_airport_id: Airport.find_by!(name: "SFO").id,
+              arriving_airport_id: Airport.find_by!(name: "ORD").id,
+              takeoff: '2021-12-9T04:05:06+00:00'
+            },
+            {
+              departing_airport_id: Airport.find_by!(name: "JFK").id,
+              arriving_airport_id: Airport.find_by!(name: "ORD").id,
+              takeoff: '2021-12-11T09:02:01+00:00'
+            }
+          ])
 
 p "Can you see the first flight? #{flights.first}"
 p "First flight departing airport: #{flights.first.departing_airport_id}"
@@ -30,5 +38,16 @@ p "First flight arriving airport: #{flights.first.arriving_airport_id}"
 p "Can you see the airport object for first flight departing airport: #{Airport.find_by!(id: flights.first.departing_airport_id)}"
 p "Does it match the airport found by name? #{Airport.find_by!(name: "SFO")}"
 p "Does it match the airport found by AGAIN name? #{Airport.find_by!(name: "SFO")}"
-
 p "How about just saying departing airport! and getting its name? #{flights.first.departing_airport.name}"
+puts
+p "Does first flight takeoff show anything?: #{flights.first.takeoff}"
+puts
+p "Can you see the last flight? #{flights.last}"
+p "Last flight departing airport: #{flights.last.departing_airport_id}"
+p "Last flight arriving airport: #{flights.last.arriving_airport_id}"
+p "Can you see the airport object for last flight departing airport: #{Airport.find_by!(id: flights.last.departing_airport_id)}"
+p "Does it match the airport found by name? #{Airport.find_by!(name: "JFK")}"
+p "Does it match the airport found by AGAIN name? #{Airport.find_by!(name: "ORD")}"
+p "How about just saying departing airport! and getting its name? #{flights.last.departing_airport.name}"
+puts
+p "Does last flight takeoff show anything?: #{flights.last.takeoff}"
