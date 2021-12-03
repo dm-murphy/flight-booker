dca550db2d931f1862474aa97ecc025f596bd8ac
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-03 15:55:39 -0500

    Add Flight method to format datetime

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: "Airport", foreign_key: "departing_airport_id"
   belongs_to :arriving_airport, class_name: "Airport", foreign_key: "arriving_airport_id"
+
+  def takeoff_formatted
+    takeoff.strftime("%a %m/%d/%Y %k:%I %p")
+  end
 end
