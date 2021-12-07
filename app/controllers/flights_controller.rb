c77338e1ef19486e427374e2979fcbd1025ff579
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-07 13:15:55 -0500

    Add #search_params and @searched_flights to controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @searched_flights = Flight.find(search_params)
+  end
+
+  def search_params
+    params.require(:flight).permit(:departing_airport_id, :arriving_airport_id, :passengers, :takeoff)
   end
 end
