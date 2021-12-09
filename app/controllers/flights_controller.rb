fcbec89a90f8b675149f2bc1d17dc15b3278b9aa
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-09 09:00:36 -0500

    Add @number_passengers to Flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,8 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     if params[:flight]
-      
       @searched_flights = Flight.where(flight_params)
+      @number_passengers = passenger_params
     end
   end
 
@@ -13,7 +13,12 @@ class FlightsController < ApplicationController
     params.require(:flight).permit(:departing_airport_id, :arriving_airport_id, :takeoff)
   end
 
+  # Needed anymore?
   def search_params
     params.permit(:departing_airport_id, :arriving_airport_id, :passengers, :takeoff)
+  end
+
+  def passenger_params
+    params[:flight][:passengers]
   end
 end
