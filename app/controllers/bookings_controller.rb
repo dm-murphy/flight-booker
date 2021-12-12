5a01adf27fbb2cc209a640fa6dda945feed86889
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-12 16:06:51 -0500

    Add initial methods for BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,24 @@ class BookingsController < ApplicationController
   def index
     @bookings = Book.all
   end
+
+  def new
+    @booking = Booking.new(booking_params)
+  end
+
+  # def create
+  #   @booking = Booking.new(booking_params)
+
+  #   if @booking.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:passengers, :flight_id)
+    end
 end
