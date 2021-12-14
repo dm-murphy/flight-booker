456637174fd492b876b514b3565a8c7bc5064ea7
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-14 17:11:49 -0500

    Update #new with separate methods for passenger and flight params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,22 +4,22 @@ class BookingsController < ApplicationController
   end
 
   def new
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
+    @flight = flight_finder
+    @number_passengers = passenger_params
   end
-
-  # def create
-  #   @booking = Booking.new(booking_params)
-
-  #   if @booking.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
 
   private
 
     def booking_params
       params.require(:booking).permit(:passengers, :flight_id)
+    end
+
+    def flight_finder
+      Flight.find(params[:booking][:flight_id])
+    end
+
+    def passenger_params
+      params[:booking][:passengers]
     end
 end
