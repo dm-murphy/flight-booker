9c1defc32405134ddc4fb3727ccab4fd2da9d64e
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-02-09 16:22:38 -0500

    Add tests for invalid inputs in create_booking_spec.rb

--- spec/system/create_booking_spec.rb
+++ spec/system/create_booking_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe 'Open new booking form after flight search submission', type: :sy
     click_on 'Submit'
     expect(page).to have_content('New Booking Form')
   end
+
+  scenario 'invalid inputs from matching airport ids' do
+    Rails.application.load_seed
+    visit flights_path
+    select "SFO", :from => "flight[departing_airport_id]"
+    select "SFO", :from => "flight[arriving_airport_id]"
+    select "1", :from => "flight[passengers]"
+    date = Time.zone.now + 1.days
+    formatted_date = date.strftime("%a %m/%d/%Y")
+    select formatted_date, :from => "flight[takeoff_date]"
+    click_on 'Search'
+    expect(page).not_to have_content('Submit')
+  end
+
+  scenario 'invalid inputs from blank form fields' do
+    Rails.application.load_seed
+    visit flights_path
+    click_on 'Search'
+    expect(page).not_to have_content('Submit')
+  end
 end
