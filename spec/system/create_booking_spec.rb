668d2046077a9aaabdacaf46165aac6b9cec26d1
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-02-13 11:58:04 -0500

    Add spec scenario for invalid booking without all passenger info

--- spec/system/create_booking_spec.rb
+++ spec/system/create_booking_spec.rb
@@ -66,8 +66,24 @@ RSpec.describe 'Open new booking form after flight search submission', type: :sy
     click_on 'Search'
     choose(name: 'booking[flight_id]', match: :first)
     click_on 'Submit'
-    # fill_in 'Full name:', with: 'Uncle Bob'
-    # fill_in 'Email:', with: 'cleancoderocks@fakemail.com'
+    click_on 'Create Booking'
+    expect(page).not_to have_content('SUCCESS')
+  end
+
+  scenario 'require all passenger fields to be entered' do
+    Rails.application.load_seed
+    visit flights_path
+    select "SFO", :from => "flight[departing_airport_id]"
+    select "JFK", :from => "flight[arriving_airport_id]"
+    select "3", :from => "flight[passengers]"
+    date = Time.zone.now + 1.days
+    formatted_date = date.strftime("%a %m/%d/%Y")
+    select formatted_date, :from => "flight[takeoff_date]"
+    click_on 'Search'
+    choose(name: 'booking[flight_id]', match: :first)
+    click_on 'Submit'
+    fill_in 'Full name:', with: 'Uncle Bob', match: :first
+    fill_in 'Email:', with: 'cleancoderocks@fakemail.com', match: :first
     click_on 'Create Booking'
     expect(page).not_to have_content('SUCCESS')
   end
