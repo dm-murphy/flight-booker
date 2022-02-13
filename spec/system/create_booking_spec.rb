88c14029de95d0ee7abc14b4ca876b3863b9e297
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-02-13 11:43:30 -0500

    Expand spec scenarios for booking input fields

--- spec/system/create_booking_spec.rb
+++ spec/system/create_booking_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Open new booking form after flight search submission', type: :system do
-  scenario 'valid inputs' do
+  scenario 'valid search inputs' do
     Rails.application.load_seed
     visit flights_path
     select "SFO", :from => "flight[departing_airport_id]"
@@ -16,7 +16,25 @@ RSpec.describe 'Open new booking form after flight search submission', type: :sy
     expect(page).to have_content('New Booking Form')
   end
 
-  scenario 'invalid inputs from matching airport ids' do
+  scenario 'valid search and booking inputs' do
+    Rails.application.load_seed
+    visit flights_path
+    select "SFO", :from => "flight[departing_airport_id]"
+    select "JFK", :from => "flight[arriving_airport_id]"
+    select "1", :from => "flight[passengers]"
+    date = Time.zone.now + 1.days
+    formatted_date = date.strftime("%a %m/%d/%Y")
+    select formatted_date, :from => "flight[takeoff_date]"
+    click_on 'Search'
+    choose(name: 'booking[flight_id]', match: :first)
+    click_on 'Submit'
+    fill_in 'Full name:', with: 'Uncle Bob'
+    fill_in 'Email:', with: 'cleancoderocks@fakemail.com'
+    click_on 'Create Booking'
+    expect(page).to have_content('SUCCESS')
+  end
+
+  scenario 'invalid search inputs from matching airport ids' do
     Rails.application.load_seed
     visit flights_path
     select "SFO", :from => "flight[departing_airport_id]"
@@ -29,10 +47,28 @@ RSpec.describe 'Open new booking form after flight search submission', type: :sy
     expect(page).not_to have_content('Submit')
   end
 
-  scenario 'invalid inputs from blank form fields' do
+  scenario 'invalid search inputs from blank form fields' do
     Rails.application.load_seed
     visit flights_path
     click_on 'Search'
     expect(page).not_to have_content('Submit')
+  end
+
+  scenario 'valid search inputs but invalid booking inputs' do
+    Rails.application.load_seed
+    visit flights_path
+    select "SFO", :from => "flight[departing_airport_id]"
+    select "JFK", :from => "flight[arriving_airport_id]"
+    select "1", :from => "flight[passengers]"
+    date = Time.zone.now + 1.days
+    formatted_date = date.strftime("%a %m/%d/%Y")
+    select formatted_date, :from => "flight[takeoff_date]"
+    click_on 'Search'
+    choose(name: 'booking[flight_id]', match: :first)
+    click_on 'Submit'
+    # fill_in 'Full name:', with: 'Uncle Bob'
+    # fill_in 'Email:', with: 'cleancoderocks@fakemail.com'
+    click_on 'Create Booking'
+    expect(page).not_to have_content('SUCCESS')
   end
 end
